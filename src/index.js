f38511b18b50ee5fa0eb75dc72b3a45127de986f
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2023-03-28 18:11:54 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,62 +1,95 @@
-const contentDiv = document.getElementById('content');
-// create header element
-const header = document.createElement('header');
-
-// create heading element and add to header
-const heading = document.createElement('h1');
-heading.textContent = 'Restaurant Name';
-header.appendChild(heading);
-
-// create navigation element and add to header
-const nav = document.createElement('nav');
-const ul = document.createElement('ul');
-const menuLink = document.createElement('a');
-menuLink.href = '#';
-menuLink.textContent = 'Menu';
-const aboutLink = document.createElement('a');
-aboutLink.href = '#';
-aboutLink.textContent = 'About Us';
-const contactLink = document.createElement('a');
-contactLink.href = '#';
-contactLink.textContent = 'Contact';
-const menuListItem = document.createElement('li');
-menuListItem.appendChild(menuLink);
-const aboutListItem = document.createElement('li');
-aboutListItem.appendChild(aboutLink);
-const contactListItem = document.createElement('li');
-contactListItem.appendChild(contactLink);
-ul.appendChild(menuListItem);
-ul.appendChild(aboutListItem);
-ul.appendChild(contactListItem);
-nav.appendChild(ul);
-header.appendChild(nav);
-
-// create main element
-const main = document.createElement('main');
-
-// create image element and add to main
-const img = document.createElement('img');
-img.src = 'https://example.com/images/restaurant.jpg';
-img.alt = 'Restaurant Name';
-main.appendChild(img);
-
-// create heading and paragraph elements and add to main
-const mainHeading = document.createElement('h2');
-mainHeading.textContent = 'Welcome to Restaurant Name';
-const mainParagraph = document.createElement('p');
-mainParagraph.textContent = 'We offer the finest dining experience with a wide selection of delicious dishes to choose from. Our chefs use only the freshest ingredients to prepare our meals, and our friendly staff will ensure that you have a memorable dining experience.';
-main.appendChild(mainHeading);
-main.appendChild(mainParagraph);
-
-// create footer element
-const footer = document.createElement('footer');
-
-// create paragraph element and add to footer
-const footerParagraph = document.createElement('p');
-footerParagraph.textContent = '\u00A9 2023 Restaurant Name';
-footer.appendChild(footerParagraph);
-
-// add header, main, and footer to body
-contentDiv.appendChild(header);
-contentDiv.appendChild(main);
-contentDiv.appendChild(footer);
+const menu = {
+    beverages: [
+      {
+        name: 'Honey Tea',
+        description: 'A warm tea made with honey and lemon',
+        price: '$3.50',
+      },
+      {
+        name: 'Beary Tea',
+        description: 'A cold tea infused with berry flavors',
+        price: '$4.00',
+      },
+    ],
+    sides: [
+      {
+        name: 'Toast and Jam',
+        description: 'A slice of toast with homemade blackberry or raspberry jam',
+        price: '$2.50',
+      },
+      {
+        name: 'Fresh Fruit',
+        description: 'A small bowl of fresh fruit',
+        price: '$3.00',
+      },
+    ],
+    mainDishes: [
+      {
+        name: 'Pancakes',
+        description: 'A stack of homemade buttermilk pancakes served with locally sourced maple syrup',
+        price: '$6.00',
+      },
+      {
+        name: 'French Toast',
+        description: 'Two slices of french toast served with locally sourced maple syrup',
+        price: '$7.00',
+      },
+      {
+        name: 'Beary Veggie Sandwich',
+        description: 'A sandwich filled with fresh vegetables',
+        price: '$8.50',
+      },
+      {
+        name: 'BLT',
+        description: 'A sandwich with bacon, lettuce, and tomato',
+        price: '$9.00',
+      },
+      {
+        name: 'Bagel and Lox',
+        description: 'A bagel topped with sustainably harvested salmon',
+        price: '$11.00',
+      },
+    ],
+  };
+  
+  const contentDiv = document.getElementById('content');
+  
+  // create a menu section with a title and items
+  function createMenuSection(title, items) {
+    const section = document.createElement('section');
+    const heading = document.createElement('h2');
+    const list = document.createElement('ul');
+  
+    heading.textContent = title;
+    section.appendChild(heading);
+  
+    items.forEach(item => {
+      const listItem = document.createElement('li');
+      const name = document.createElement('h3');
+      const description = document.createElement('p');
+      const price = document.createElement('span');
+  
+      name.textContent = item.name;
+      description.textContent = item.description;
+      price.textContent = item.price;
+  
+      listItem.appendChild(name);
+      listItem.appendChild(description);
+      listItem.appendChild(price);
+      list.appendChild(listItem);
+    });
+  
+    section.appendChild(list);
+    return section;
+  }
+  
+  // create menu sections for each category
+  const beveragesSection = createMenuSection('Beverages', menu.beverages);
+  const sidesSection = createMenuSection('Sides', menu.sides);
+  const mainDishesSection = createMenuSection('Main Dishes', menu.mainDishes);
+  
+  // append the menu sections to the content div
+  contentDiv.appendChild(beveragesSection);
+  contentDiv.appendChild(sidesSection);
+  contentDiv.appendChild(mainDishesSection);
+  
